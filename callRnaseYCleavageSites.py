5a06ce7f9c4801f3a5a8be3583d596443c3c3875
ethanagbaker <ethanagbaker@gmail.com>
2018-04-30 14:02:47 -0400

    untested file import complete

--- callRnaseYCleavageSites.py
+++ callRnaseYCleavageSites.py
@@ -20,9 +20,36 @@ with open (cwd + '/rnaseYNames.txt', 'r') as f:
 with open (cwd + '/5exoNames.txt', 'r') as f:
     fiveExoFiles = f.readlines()
 
-
 #parse wigs
+wtData = pd.DataFrame()
+pnpaData = pd.DataFrame()
+rnaseJData = pd.DataFrame()
+rnaseYFiles = pd.DataFrame()
+fiveExoFiles = pd.DataFrame()
+
+for name in wtFiles:
+    d = parseWig(name)
+    wtData.append(d)
+
+for name in pnpaData:
+    d = parseWig(name)
+    pnpaData.append(d)
+
+for name in rnaseJData:
+    d = parseWig(name)
+    rnaseJData.append(d)
+
+for name in rnaseYFiles:
+    d = parseWig(name)
+    rnaseYData.append(d)
+
+for name in fiveExoFiles:
+    d = parseWig(name)
+    fiveExoFiles.append(d)
 
 #import annotation file.
+annotationFile = '/Users/eagb/Dropbox (MIT)/LiLab/For_Ethan_B_subtilis_nucleases/B_subtilis/CDS_168_detailed_Mochi_20180411.txt'
+
+forwardFeatures, reverseFeatures = parseAnnotation(annotationFile)
 
 #call peaks and sort by gene
